8f0f116ed6d9de95c03846d09133cdb4097964fc
Jake Holtz <jahholtz@gmail.com>
2017-04-29 14:21:41 -0700

    Correctly handling 'Not in Database' instance

--- server/index.js
+++ server/index.js
@@ -8,19 +8,21 @@ app.use(express.static(__dirname + '/../react-client/dist'));
 app.use(bodyParser.json())
 
 app.post('/search', function (req, res) {
+
   var country = req.body.country.toUpperCase();
-  var found;
 
   console.log('CLIENT -> SERVER [Post Received]');
 
   db.query(`SELECT capital FROM countries WHERE country = "${country}";`, function(error, success) {
-      if(error) {
-        console.log("Not a country!")
-      } else {
-        console.log("Capital successfully retrieved");
-        res.send(success[0].capital)
-      }
-    });
+    if(error) {
+      console.log("Capital retrieving error!");
+    } else if (success.length === 0) {
+      res.send("NONE. NOT A COUNTRY!");
+    } else {
+      console.log(success[0].capital);
+      res.send(success[0].capital);
+    }
+  });
 });
 
 app.listen(3000, function() {
